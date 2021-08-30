3f58d009981c7e5dabfd741fb29f06fc595a64a4
Vygoposha <iavygolovskiy@gmail.com>
2021-08-30 21:02:52 +0800

    repair urls/tasks

--- news/urls.py
+++ news/urls.py
@@ -7,15 +7,15 @@ from allauth.account.views import LogoutView
 urlpatterns = [
     path('', IndexView.as_view()),
     # path — означает путь. В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно почему
-    # path('', NewsList.as_view(), name = 'news'),
-    # # т.к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
-    # path('<int:pk>/', NewDetailView.as_view(), name = 'detail_news'),
-    # path('search/', NewsSearch.as_view(), name = 'search'),
-    # path('add/', NewsCreate.as_view(), name ='create_news'),
-    # path('post/<int:pk>/edit/', NewEdit.as_view(), name ='edit_news'),
-    # path('post/<int:pk>/delete/', NewDelete.as_view(), name ='delete_news'),
-    # path('upgrade/', upgradeMe, name = 'upgrade'),
-    # path('subscribe/<int:pk>/', Subscribe.as_view(), name = 'subscribe'),
+    path('', NewsList.as_view(), name = 'news'),
+    # т.к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
+    path('<int:pk>/', NewDetailView.as_view(), name = 'detail_news'),
+    path('search/', NewsSearch.as_view(), name = 'search'),
+    path('add/', NewsCreate.as_view(), name ='create_news'),
+    path('post/<int:pk>/edit/', NewEdit.as_view(), name ='edit_news'),
+    path('post/<int:pk>/delete/', NewDelete.as_view(), name ='delete_news'),
+    path('upgrade/', upgradeMe, name = 'upgrade'),
+    path('subscribe/<int:pk>/', Subscribe.as_view(), name = 'subscribe'),
 
 
 
